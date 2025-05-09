ebdc1d59d406e0b1cc8d2089f411c94e255cea6d
anji0114 <anji.tnk.140114@gmail.com>
2025-05-10 00:37:03 +0900

    delete nest

--- backend/src/hello/hello.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class HelloService {
-  getHello(name: string | undefined): string {
-    return `Hello ${name || 'unnamed'}`;
-  }
-}
