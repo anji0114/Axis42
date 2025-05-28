83f5c267fe73faa4aad11ccc2a41b34d9606b05e
anji0114 <anji.tnk.140114@gmail.com>
2025-05-28 09:37:39 +0900

    user

--- backend/src/users/users.resolver.ts
+++ backend/src/users/users.resolver.ts
@@ -1,4 +1,4 @@
-import { Resolver, Query, Args, Int } from '@nestjs/graphql';
+import { Resolver, Query, Args, Int, Mutation } from '@nestjs/graphql';
 import { UsersService } from './users.service';
 import { User } from './entities/user.entity';
 
@@ -14,5 +14,13 @@ export class UsersResolver {
   @Query(() => User)
   user(@Args('id', { type: () => Int }) id: number): User {
     return this.usersService.findOne(id);
+  }
+
+  @Mutation(() => User)
+  updateUsername(
+    @Args('id', { type: () => Int }) id: number,
+    @Args('username') username: string,
+  ): User {
+    return this.usersService.updateUsername(id, username);
   }
 }
